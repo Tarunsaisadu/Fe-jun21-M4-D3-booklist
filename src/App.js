ea3ca4e927bb20e96a522f861832432310e98007
Tarunsai Sadu <tarunsai.rct@gmail.com>
2021-08-25 16:50:47 +0200

    user click for displaying books

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import WarningSign from "./components/WarningSign";
+
+function App() {
+  return (
+    <div className="App">
+      <WarningSign />
+    </div>
+  );
+}
+
+export default App;
